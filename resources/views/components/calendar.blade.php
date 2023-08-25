9bc28d9190c2f01c5a48a73de140229aa76fe560
akichi139 <supphakit.c@ku.th>
2023-08-25 11:47:28 +0700

    event calendar blade component

---
+++ resources/views/components/calendar.blade.php
@@ -0,0 +1,39 @@
+@props(['events' => ''])
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.js"></script>
+
+<div id="calendar"></div>
+
+<script>
+    $(document).ready(function () {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        var calendar = $('#calendar').fullCalendar({
+            eventRender: function (event, element) {
+                element.attr('title' + event.participant);
+                element.tooltip({
+                    container: 'body',
+                    delay: {
+                        show: 500,
+                        hide: 0
+                    }
+                });
+            },
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay'
+            },
+            events: @json($events),
+            displayEventTime: false,
+            selectable: true,
+            selectHelper: true,
+        });
+    });
+</script>
